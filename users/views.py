d0df716f01f759ddf42ba6332139b493e45a1b06
Anastasia-l <anastasiazorina2299@gmail.com>
2025-05-31 11:53:08 +0300

    Updated views.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
-from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from .models import CustomUser
+
 
 # Create your views here.
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
@@ -11,7 +15,7 @@ def register(request):
             user = form.save()
             login(request, user)
             return redirect('courses:course_list')
-        
+
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
@@ -24,11 +28,22 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('courses:course_list')
+            return redirect('courses:course_list')  # Добавить courses:
     return render(request, 'users/login.html')
+
 
 def logout_view(request):
     logout(request)
     return redirect('users:login')
-        
+
+
+@login_required
+def profile(request):
+    user = request.user
+    context = {
+        'user': user,
+        'role': user.get_role_display()
+    }
+    return render(request, 'users/profile.html', context)
+
             
